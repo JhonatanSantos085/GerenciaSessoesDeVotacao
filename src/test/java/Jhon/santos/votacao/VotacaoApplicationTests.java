fc767e25fbf50e85741a8d50dff71a73176db780
Jhonatan Santos <jhonatansantos085@gmail.com>
2024-07-28 18:49:57 -0300

    commit inicial

---
+++ src/test/java/Jhon/santos/votacao/VotacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package Jhon.santos.votacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
